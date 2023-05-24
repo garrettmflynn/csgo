23db7d51c489fe881af82b409f12fa25524932d5
Garrett <gflynn@usc.edu>
2023-05-24 14:31:05 -0700

    Assign IDs to specific updates and trigger only on those. Separate previously and added

--- src/config.js
+++ src/config.js
@@ -31,15 +31,15 @@ export const template = {
 		"player_match_stats": 		"1",	// scoreboard info
 		"player_weapons":			"1", // list of player weapons and weapon state
 
-        // Match Observer
-		"allplayers_id":       		"1", // the steam id of each player
-		"allplayers_state":    		"1", // the player_state for each player 
-		"allplayers_match_stats":  	"1", // the scoreboard info for each player
-		"allplayers_weapons":  		"1", // the player_weapons for each player
-		"allplayers_position": 		"1", // player_position but for each player
-		"phase_countdowns":    		"1", // time remaining in tenths of a second, which phase
-		"allgrenades":         		"1", // grenade effecttime, lifetime, owner, position, type, velocity
-		"bomb":                		"1", // location of the bomb, who's carrying it, dropped or not
+        // // Match Spectators Only
+		// "allplayers_id":       		"1", // the steam id of each player
+		// "allplayers_state":    		"1", // the player_state for each player 
+		// "allplayers_match_stats":  	"1", // the scoreboard info for each player
+		// "allplayers_weapons":  		"1", // the player_weapons for each player
+		// "allplayers_position": 		"1", // player_position but for each player
+		// "phase_countdowns":    		"1", // time remaining in tenths of a second, which phase
+		// "allgrenades":         		"1", // grenade effecttime, lifetime, owner, position, type, velocity
+		// "bomb":                		"1", // location of the bomb, who's carrying it, dropped or not
 		"player_position":     		"1", // forward direction, position for currently spectated player
 	}
 }
